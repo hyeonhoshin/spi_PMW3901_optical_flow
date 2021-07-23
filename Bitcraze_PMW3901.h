db20845875b861fca10ded6e5ce6773988e11454
hyeonhoshin <tlsgusghq@kaist.ac.kr>
2021-07-23 23:48:49 +0900

    그래도 오류가 안잡혀서 기존꺼 따라가봄.

--- Bitcraze_PMW3901.h
+++ Bitcraze_PMW3901.h
@@ -6,8 +6,8 @@
 extern int Bitcraze_PMW3901_init(int);
 extern void Bitcraze_PMW3901_readMotionCount(int, int16_t*, int16_t*);
 
-extern void enableFrameBuffer(int fd);
-extern void readFrameBuffer(int fd, char *FBuffer);
+extern void enableFrameBuffer(int);
+extern void readFrameBuffer(int, char*);
 
 #endif // __SPI_PMW3901
 
